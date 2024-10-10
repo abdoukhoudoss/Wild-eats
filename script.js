79ef957e2de6d5920334df71532614ea919d0b12
TSMB54 <tom.schricke@gmail.com>
2024-10-10 10:52:08 +0200

    header

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 import data from './data.json' with {type: "json"};
 
+const bountonfiltre = document.querySelector(".boutonfiltre")
+const depliant = document.querySelector(".filtre")
+
+bountonfiltre.addEventListener('click', (a) => {
+    a.preventDefault();
+    depliant.classList.toggle("avec")
+})
 
